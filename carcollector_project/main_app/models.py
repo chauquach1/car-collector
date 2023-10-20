9990bfb6bb7d74804651ffd5e5588591bd218be0
chauquach1 <chau268@gmail.com>
2023-10-19 18:28:03 -0700

    added delete and update cars feature

--- carcollector_project/main_app/models.py
+++ carcollector_project/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
+class Car(models.Model):
+  make = models.CharField(max_length=25)
+  model = models.CharField(max_length=25)
+  year = models.IntegerField(default=timezone.now().year)
+  color = models.CharField(max_length=25)
+  nickname = models.CharField(max_length=25, default='') 
+
+  def __str__(self):
+    return f"{self.nickname}: {self.make} {self.model}"
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'car_id': self.id})
--- carcollector_project/main_app/views.py
+++ carcollector_project/main_app/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+# Add UpdateView & DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Car
+
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'cars/home.html')
+
+# Define the about view
+def about(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'cars/about.html')
+
+def cars_index(request):
+  # We pass data to a template very much like we did in Express!
+  cars = Car.objects.all()
+  return render(request, 'cars/index.html', { 'cars': cars })
+
+def cars_detail(request, car_id):
+  car = Car.objects.get(id=car_id)
+  return render(request, 'cars/detail.html', { 'car': car })
+
+class CarCreate(CreateView):
+  model = Car
+  fields = '__all__'
+  # Special string pattern Django will use
+  def get_success_url(self):
+      # Use reverse to generate the URL based on the car_id of the newly created Car instance
+      return reverse('car_detail', args=[self.object.id])
+class CarUpdate(UpdateView):
+  model = Car
+  # Let's disallow the renaming of a car by excluding the name field!
+  fields = '__all__'
+  
+class CarDelete(DeleteView):
+  model = Car
+  success_url = '/cars'
